80be1996253670df04ee03c19ea479c8e19f12f2
James Boyle <james.patrick.boyle92@gmail.com>
2023-05-23 21:43:06 +0200

    Format indentation

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,125 +1,126 @@
 import axios from "axios"
 import { useState, useContext, createContext, useEffect } from "react"
-import { useNavigate, Navigate } from "react-router"
+import { useNavigate } from "react-router"
 
 interface AuthProviderProps {
-    children: React.ReactNode
+  children: React.ReactNode
 }
 
 interface User {
-    userId: number
-    name: string
+  userId: number
+  name: string
 }
 
 export const AuthContext = createContext<any>(null)
 
 export default function AuthContextProvider({children}: AuthProviderProps) {
 
-    const navigate = useNavigate()
+  const navigate = useNavigate()
 
-    const [user, setUser] = useState<User | null>(null)
+  const [user, setUser] = useState<User | null>(null)
 
-    useEffect(() => {
-        const token = localStorage.getItem("token")
-        if (token) {
-        // Extract user information from the token
+  useEffect(() => {
+    const token = localStorage.getItem("token")
+    if (token) {
+    // Extract user information from the token
+    const decodedToken = decodeToken(token)
+    if (decodedToken) {
+      const { userId, name } = decodedToken
+      setUser({ userId, name })
+    }
+    }
+  }, [])
+  
+  const login = async (email: string, password: string) => {
+
+    try {
+      const response = await axios.post(
+        "http://localhost:5000/api/users/login",
+        {
+          email,
+          password,
+        }
+      )
+
+      const { token } = response.data
+
+      if (token) {
+        localStorage.setItem("token", token)
         const decodedToken = decodeToken(token)
         if (decodedToken) {
-            const { userId, name } = decodedToken
-            setUser({ userId, name })
+          const { userId, name } = decodedToken
+          setUser({ userId, name })
         }
+        
+        navigate("/dashboard")
+      }
+    } catch (error) {
+      console.error(error)
+    }
+  }
+  
+  const register = async (name: string, email: string, password: string) => {
+
+    try {
+      const response = await axios.post("http://localhost:5000/api/users/register", {
+        name,
+        email,
+        password,
+      })
+      
+      const { token } = response.data
+
+      console.log(response)
+
+      if (token) {
+        localStorage.setItem("token", token)
+        const decodedToken = decodeToken(token)
+        if (decodedToken) {
+          const { userId, name } = decodedToken
+          setUser({ userId, name })
         }
-    }, [])
-    
-    const login = async (email: string, password: string) => {
-        try {
-          const response = await axios.post(
-            "http://localhost:5000/api/users/login",
-            {
-              email,
-              password,
-            }
-          )
-    
-          const { token } = response.data
-    
-          if (token) {
-            localStorage.setItem("token", token)
-            const decodedToken = decodeToken(token)
-            if (decodedToken) {
-              const { userId, name } = decodedToken
-              setUser({ userId, name })
-            }
-            
-            navigate("/dashboard")
-          }
-        } catch (error) {
-          console.error(error)
-        }
+        
+        navigate("dashboard")
+      }
+    } catch (error) {
+      console.error(error)
     }
-    
-    const register = async (name: string, email: string, password: string) => {
+  }
 
-        try {
-          const response = await axios.post("http://localhost:5000/api/users/register", {
-            name,
-            email,
-            password,
-          })
-          
-          const { token } = response.data
+  const logout = () => {
+    localStorage.removeItem("token")
+    setUser(null)
+    navigate("/login")
+  }
 
-          console.log(response)
-    
-          if (token) {
-            localStorage.setItem("token", token)
-            const decodedToken = decodeToken(token)
-            if (decodedToken) {
-              const { userId, name } = decodedToken
-              setUser({ userId, name })
-            }
-            
-            navigate("dashboard")
-          }
-        } catch (error) {
-          console.error(error)
-        }
-    }
+  const value = {
+    user,
+    setUser,
+    login,
+    register,
+    logout
+  }
 
-    const logout = () => {
-        localStorage.removeItem("token")
-        setUser(null)
-        navigate("/login")
-    }
-
-    const value = {
-        user,
-        setUser,
-        login,
-        register,
-        logout
-    }
-
-    return (
-        <AuthContext.Provider value={value}>
-            {children}
-        </AuthContext.Provider>
-    )
+  return (
+    <AuthContext.Provider value={value}>
+        {children}
+    </AuthContext.Provider>
+  )
 }
 
 export const useAuth = () => useContext(AuthContext)
 
 // Helper function to decode the token and extract user information
 function decodeToken(token: string) {
-    try {
-      // Assuming the token is a JWT (JSON Web Token)
-      const payloadBase64 = token.split(".")[1]
-      const payloadJson = atob(payloadBase64)
-      const payload = JSON.parse(payloadJson)
-      return payload
-    } catch (error) {
-      console.error("Failed to decode token:", error)
-      return null
-    }
+  try {
+    // Assuming the token is a JWT (JSON Web Token)
+    const payloadBase64 = token.split(".")[1]
+    const payloadJson = atob(payloadBase64)
+    const payload = JSON.parse(payloadJson)
+    return payload
+  } catch (error) {
+    console.error("Failed to decode token:", error)
+    return null
   }
+}
 
